22d043af2429fbd89398ff20c2cb4a4e96fec105
dbaluku <juniorderrick18@gmail.com>
2019-09-20 17:25:52 +0300

    Added js to disable and enable other fields depending on their triggers

--- omod/src/main/webapp/resources/js/ugandaemr-poc.js
+++ omod/src/main/webapp/resources/js/ugandaemr-poc.js
@@ -1,4 +1,34 @@
 
+function disable_fields(groupId){
+    var disable = true;
+    var group = jq("#"+groupId);
+    group.find("input").attr("disabled", disable);
+    group.find('select').attr("disabled", disable);
+
+    if (disable) {
+        /* fade out the fields that are disabled */
+        group.find("input").fadeTo(250, 0.25);
+        group.find("select").fadeTo(250, 0.25);
+    } else {
+        /* remove the fade on the fields */
+        group.find("input").fadeTo(250, 1);
+        group.find("select").fadeTo(250, 1);
+    }
+}
+
+function enable_fields(group){
+    var disable = false;
+    var group = jq("#"+group);
+    group.find("input").attr("disabled", disable);
+    group.find('select').attr("disabled", disable);
+
+    if (!disable) {
+        /* remove the fade on the fields */
+        group.find("input").fadeTo(250, 1);
+        group.find("select").fadeTo(250, 1);
+    }
+}
+
 function enable_disable_fm(selected_option) {
 
     var class_name = jq(selected_option).attr("class");
